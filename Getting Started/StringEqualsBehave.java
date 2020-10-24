f20e0439e721eb80823698513f7e9ab35944d49f
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-10-24 15:21:50 +0530

    change for the check of github synchro

--- Getting Started/StringEqualsBehave.java
+++ Getting Started/StringEqualsBehave.java
@@ -3,34 +3,34 @@ package strings;
 public class StringEqualsBehave {
 
 	public static void main(String[] args) {
-				
+
 				String s1 = "Hello";
 				String s2 = "Hello";       // "Hello" will be stored in 1 place only and its address will be
 				                           // given to both s1 and s2
-				
+
 				s1 = s1 + 'e';            // a new memory will be used where "Hello" will be copied and 'e' will be added to it.
-										  // THEREFORE TIME COMPLEXITY : O(n) 
-				
+										  // THEREFORE TIME COMPLEXITY : O(n)
+
 				s1 = "Hello";
 				// To store "Hello" in new place we have to use keyword : new
-				
-				
+
+
 				String s3 = new String("Hello");
-				
+
 				// == sign only compares the address :
 
 				System.out.println(s1 == s2);
 
 				System.out.println(s1 == s3);
-				
-				// to compare the characters we use "equals" :
-				
-				System.out.println(s1.equals(s3));
-				 
 
-				
-		
-	
+				// to compare the characters we use "equals" :
+
+				System.out.println(s1.equals(s3));
+				 //to check not an actual change
+
+
+
+
 
 	}
 
