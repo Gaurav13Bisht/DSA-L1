7c4c9a5eaeaa9b84b6732e4db5d17d58b8f3b443
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-10-29 13:36:00 +0530

    Added PATTERN 4 AND 5 program code in patterns.java file

--- Basics/Patterns.java
+++ Basics/Patterns.java
@@ -13,6 +13,8 @@ public class Main {
         pattern1();
         pattern2();
         pattern3();
+        pattern4();
+        pattern5();
 
 
     }
@@ -68,7 +70,7 @@ public class Main {
                   *	*	*	*
                 *	*	*	*	*
         */
-          
+
           int n = scn.nextInt();
           for (int i = 1; i <= n; i++) {
               for (int j = 1; j <= n; j++)
@@ -78,10 +80,84 @@ public class Main {
                     System.out.print("	");
 
             System.out.println();
-        }
+          }
 
 
-    }
+      }
+
+
+      public static void pattern4() {
+       /*   5
+            *	*	*	*	*
+	            * *	*	*
+		            * *	*
+			            *	*
+				            *
+       */
+
+
+          int n = scn.nextInt();
+            /* for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++)
+                  if (j < i)
+                   System.out.print("	");
+               else
+                   System.out.print("*	");
+
+              System.out.println();
+
+
+           }
+          */
+          int sp = 0, st = n;
+          for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sp; j++)
+               System.out.print("	");
+
+           for (int j = 1; j <= st; j++)
+               System.out.print("*	");
+
+           sp++;
+           st--;
+
+           System.out.println();
+          }
+
+     }
+
+
+     public static void pattern5() {
+       /*  5
+                  *
+                *	*	*
+              *	*	*	*	*
+                *	*	*
+                  *
+      */
+        int n = scn.nextInt();
+        int st = 1 , sp = n/2;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= sp ; j++)
+                System.out.print("	");
+
+            for(int j = 1 ; j <= st ; j++)
+                System.out.print("*	");
+
+                if( i <= n/2){
+                    st+=2;
+                    sp--;
+                }
+                else{
+                    st-=2;
+                    sp++;
+                }
+
+                System.out.println();
+
+             }
+
+
+      }
 
 
 
