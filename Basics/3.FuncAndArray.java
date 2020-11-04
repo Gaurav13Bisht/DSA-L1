5b20572a29aa853884a08138a12e55224b149b22
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-04 14:32:38 +0530

    Added DECIMAL TO ANY BASE program code in FuncandArray.java

--- Basics/3.FuncAndArray.java
+++ Basics/3.FuncAndArray.java
@@ -6,17 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         solved();
-       //Unsolved();	
-
-    }							 
-												
-    public static void solved() {
-        getDigitFrequency();
-       
+       //Unsolved();
 
     }
 
-    
+    public static void solved() {
+        getDigitFrequency();
+        getValueInBase();
+
+
+    }
+
+
     public static void getDigitFrequency() {
         int n = scn.nextInt();
         int d = scn.nextInt();
@@ -42,9 +43,31 @@ public class Main {
 
 //---------------------------------------------------------------
 
-    
+
+      public static void getValueInBase() {
+
+          int n = scn.nextInt();
+          int b = scn.nextInt();
+          int dn = getValueInBaseFunc(n, b);
+          System.out.println(dn);
+      }
+
+      public static int getValueInBaseFunc(int n, int b) {
+          int rem, val = 0, i = 0;
+          while(n != 0) {
+            rem = n % b;
+            val += rem * Math.pow(10,i);
+            i ++;
+            n = n / b;
+          }
+          return val;
+
+      }
+
+//---------------------------------------------------------------
 
 
+      
 
 
 
