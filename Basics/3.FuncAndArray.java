78999be7a2a6574a9fd018771e824f93dde4a9f0
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-09 20:52:58 +0530

    Added INVERSE OF AN ARRAY program code in FuncAndArrays.java

--- Basics/3.FuncAndArray.java
+++ Basics/3.FuncAndArray.java
@@ -27,6 +27,7 @@ public class Main {
         diffOfArrays();
         reverseArray();
         rotateArray();
+        inverseArray();
 
     }
 
@@ -507,7 +508,37 @@ public class Main {
               i++;
               j--;
             }
+    }
+
+//---------------------------------------------------------------
+      public static void inverseArray() {
+          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+          int n = Integer.parseInt(br.readLine());
+          int[] a = new int[n];
+          for(int i = 0; i < n; i++){
+              a[i] = Integer.parseInt(br.readLine());
           }
+
+          int[] inv = inverseArrayFunc(a);
+
+          StringBuilder sb = new StringBuilder();
+            for(int val: inv){
+            sb.append(val + "
+            ");
+          }
+          System.out.println(sb);
+        }
+
+      public static int[] inverseArrayFunc(int[] a){
+            int[] inv = new int[a.length];
+            for(int i = 0; i < a.length; i++){
+              int v = a[i];
+              inv[v] = i;
+
+            }
+            return inv;
+    }
 
 //---------------------------------------------------------------
 
