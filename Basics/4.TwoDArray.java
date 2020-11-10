1c7e258f20f989300a309d76655a01d72860caed
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-10 20:47:12 +0530

    Added MATRIX MULTIPLICATION program code in 4.TwoDArray.java

---
+++ Basics/4.TwoDArray.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        solved();
+        //unsolved();
+    }
+
+    public static void solved() {
+        matrixMult();
+    }
+
+    public static void unsolved() {
+
+    }
+
+    public static void matrixMult() {
+
+        int n1 = scn.nextInt();
+        int m1 = scn.nextInt();
+
+        int[][] g1 = new int[n1][m1];
+
+        for (int i = 0; i < n1; i++)
+            for (int j = 0; j < m1; j++)
+                g1[i][j] = scn.nextInt();
+
+        int n2 = scn.nextInt();
+        int m2 = scn.nextInt();
+
+        int[][] g2 = new int[n2][m2];
+
+        for (int i = 0; i < n2; i++)
+            for (int j = 0; j < m2; j++)
+                g2[i][j] = scn.nextInt();
+
+        if (m1 == n2) {
+
+            int[][] prd = new int[n1][m2];
+
+            for (int i = 0; i < n1; i++) {
+                for (int j = 0; j < m2; j++) {
+                    for (int k = 0; k < m1; k++)
+                        prd[i][j] += g1[i][k] * g2[k][j];
+
+                    System.out.print(prd[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        } else
+            System.out.print("Invalid input");
+
+
+     }
+
+//-------------------------------------------------------------------
+
+
+
+
+
+
+
+}
