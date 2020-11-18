4690f901671828bc60a1e18ad146fed9801d046f
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-18 13:25:10 +0530

    Added PRINT INCREASING program code in IntroToRecursion.java

--- Recursion and Backtracking/IntroToRecursion.java
+++ Recursion and Backtracking/IntroToRecursion.java
@@ -5,36 +5,50 @@ public class Main {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        solved();
-       //Unsolved();	
-
-    }							 
-												
-    public static void solved() {
-	printDecreasing();
-       
+          solved();
+          //Unsolved();
 
     }
 
-    
+    public static void solved() {
+
+          printDecreasing();
+          printIncreasing();
+
+
+    }
+
+
     public static void printDecreasing(){
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        RFprintDecreasing(n);
+        printDecreasingRF(n);
     }
 
-    public static void RFprintDecreasing(int n){	 //!!! RFfunctionName : RF = Recursive Function !!!
-  	if(n == 0)
+    public static void printDecreasingRF(int n){	 //!!! RFfunctionName : RF = Recursive Function !!!
+  	    if(n == 0)
             return;
         System.out.println(n);
-        RFprintDecreasing(n - 1);
+        printDecreasingRF(n - 1);
     }
 
 //---------------------------------------------------------------
 
-    
+    public static void printIncreasing() {
 
+        int n = scn.nextInt();
+        printIncreasingRF(n);
+    }
 
+    public static void printIncreasingRF(int n){
+        if(n == 0)
+          return;
+        printIncreasingRF(n - 1);
+        System.out.println(n);
+
+    }
+
+//---------------------------------------------------------------
 
 
 
