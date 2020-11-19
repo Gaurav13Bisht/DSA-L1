ec741a87de5227345b0033cd070caef0512e104b
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-19 21:15:02 +0530

    Added PRINT ZIGZAG PATTERN program code in IntroToRecursion.java

--- Recursion and Backtracking/IntroToRecursion.java
+++ Recursion and Backtracking/IntroToRecursion.java
@@ -18,6 +18,7 @@ public class Main {
           factorial();
           powerLinear();
           powerLogarithmic();
+          zigZag();
 
     }
 
@@ -125,7 +126,25 @@ public class Main {
 
 //-----------------------------------------------------------------
 
+     public static void zigZag() {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        zigZagRF(n);
 
+    }
 
+    public static void zigZagRF(int n){
+
+        if(n == 0)
+            return;
+        System.out.print(n + " ");
+        zigZagRF(n - 1);
+        System.out.print(n + " ");
+        zigZagRF(n - 1);
+        System.out.print(n + " ");
+
+    }
+
+//-----------------------------------------------------------------
 
 }
