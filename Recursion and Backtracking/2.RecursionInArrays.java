57c0207bffb611b6863ac242b50f6f7e101c5bcb
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-20 23:23:49 +0530

    Added LAST INDEX OF AN ARRAY program code in 2.RecursionInArrays.java

--- Recursion and Backtracking/2.RecursionInArrays.java
+++ Recursion and Backtracking/2.RecursionInArrays.java
@@ -15,6 +15,7 @@ public class Main {
           displayArrRev();
           maxOfArray();
           firstIndex();
+          lastIndex();
     }
 
 
@@ -110,4 +111,67 @@ public class Main {
     }
 
 //-------------------------------------------------------------------
+
+    public static void lastIndex() {
+
+          int n = scn.nextInt();
+          int[] a = new int[n];
+          for(int i = 0; i < n; i++){
+              a[i] = scn.nextInt();
+            }
+
+            int x = scn.nextInt();
+            /*   First Approach!!!!!!!!!!!!!!!
+            System.out.print(lastIndex(a, n - 1, x));
+
+          }
+
+          public static int lastIndex(int[] arr, int idx, int x){
+                if(idx == -1)
+                    return -1;
+                else if(arr[idx] == x)
+                  return idx;
+
+                  int lid = lastIndex(arr, idx - 1, x);
+
+                  return lid;
+                }
+                */
+
+                //SECOND APPROACH!!!!!!!!!!!!!!!!!!
+
+                System.out.print(lastIndexRF(a, 0, x));
+
+    }
+
+     public static int lastIndexRF(int[] arr, int idx, int x){
+
+          if(idx == arr.length)
+              return -1;
+
+              int lidx = lastIndexRF(arr, idx + 1, x);
+              if(lidx == -1){
+                if(arr[idx] == x)
+                  return idx;
+
+                  else
+                  return -1;
+                }
+
+                else{
+                  return lidx;
+
+                }
+    }
+
+//---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 }
