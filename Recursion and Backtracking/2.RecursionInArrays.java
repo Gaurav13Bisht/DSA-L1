ad93a8549a9a5295c6f67683d922a2e6c047fe95
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-20 23:25:24 +0530

    Added ALL INDICES OF AN ARRAY program code in 2.RecursionInArrays.java

--- Recursion and Backtracking/2.RecursionInArrays.java
+++ Recursion and Backtracking/2.RecursionInArrays.java
@@ -16,6 +16,7 @@ public class Main {
           maxOfArray();
           firstIndex();
           lastIndex();
+          allIndices();
     }
 
 
@@ -166,12 +167,47 @@ public class Main {
 
 //---------------------------------------------------------------------
 
+    public static void allIndices() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int x = Integer.parseInt(br.readLine());
+        int[] iarr = allIndicesRF(arr, x, 0, 0);
 
+        if(iarr.length == 0){
+            System.out.println();
+            return;
+        }
 
+        for(int i = 0; i < iarr.length; i++){
+            System.out.println(iarr[i]);
+        }
+    }
 
+    public static int[] allIndicesRF(int[] arr, int x, int idx, int fsf) {
 
+        if(idx == arr.length){
+            return new int[fsf];
+        }
 
+        if(arr[idx] == x){
+            int[] indices = allIndicesRF(arr, x, idx + 1, fsf + 1);
+            indices[fsf] = idx;
+            return indices;
+
+        }
+        else{
+            int[] indices = allIndicesRF(arr, x, idx + 1, fsf);
+            return indices;
+        }
+
+    }
+
+//------------------------------------------------------------------------
 
 
 }
