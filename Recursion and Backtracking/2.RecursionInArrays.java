3ce40e2deca6db47c3a27125fdc941e7966fa4ce
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-20 23:16:40 +0530

    Added DISPLAY ARRAY program code in 2.RecursionInArrays.java

--- Recursion and Backtracking/2.RecursionInArrays.java
+++ Recursion and Backtracking/2.RecursionInArrays.java
@@ -11,11 +11,34 @@ public class Main {
     }
 
     public static void solved() {
+          displayArray();
 
-         
+
 
     }
 
 
-    
+    public static void displayArray() {
+
+          int n = scn.nextInt();
+          int[] a = new int[n];
+          for(int i = 0; i < n; i++){
+              a[i] = scn.nextInt();
+          }
+          displayArrRF(a, n - 1);
+   }
+
+    public static void displayArrRF(int[] arr, int idx){
+          if(idx == -1)
+              return;
+          displayArrRF(arr, idx - 1);
+          System.out.println(arr[idx]);
+    }
+
+//-------------------------------------------------------------------
+
+
+
+
+
 }
