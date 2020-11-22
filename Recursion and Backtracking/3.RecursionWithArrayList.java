f8ec50af2b8b23e1fc2e142900f5436ecf7dc678
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-22 12:22:03 +0530

    Added GET KPC program code in 3.RecursionWithArrayList.java.

--- Recursion and Backtracking/3.RecursionWithArrayList.java
+++ Recursion and Backtracking/3.RecursionWithArrayList.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void solved() {
         getSubsequence();
-
+        getKPC();
     }
 
 
@@ -51,5 +51,36 @@ public class Main {
     }
 
 //--------------------------------------------------------------------------
+
+    public static void getKPC() {
+
+        String str = scn.next();
+        ArrayList < String > res = getKPCRF(str);
+        System.out.print(res);
+
+    }
+    static String[] codes = {".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static ArrayList < String > getKPCRF(String str) {
+        if (str.length() == 0) {
+            ArrayList < String > bcase = new ArrayList < > ();
+            bcase.add("");
+            return bcase;
+        }
+        char ch = str.charAt(0);
+        ArrayList < String > str1 = getKPCRF(str.substring(1));
+        ArrayList < String > str3 = new ArrayList < > ();
+
+        String curr = codes[ch - '0'];
+
+        for(int i = 0; i < curr.length(); i++){
+            for(String st : str1)
+                str3.add(curr.charAt(i) + st);
+        }
+
+        return str3;
+
+    }
+
+//---------------------------------------------------------------------
 
 }
