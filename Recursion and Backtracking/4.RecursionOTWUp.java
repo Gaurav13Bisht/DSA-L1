cb6fceae0f0e88164d9c2903dee5232235de6eec
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-23 13:49:15 +0530

    Added PRINT SUBSEQUENCE program code in 4.RecursionOTWUp.java.

--- Recursion and Backtracking/4.RecursionOTWUp.java
+++ Recursion and Backtracking/4.RecursionOTWUp.java
@@ -11,10 +11,35 @@ public class Main {
     }
 
     public static void solved() {
-        
+
+        printSubsequence();
+
     }
 
 
+    public static void printSubsequence() {
 
-    
+        String str = scn.next();
+        printSubsequenceRF(str,"");
+    }
+
+    public static void printSubsequenceRF(String ques, String ans) {
+
+        if(ques.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = ques.charAt(0);
+
+        printSubsequenceRF(ques.substring(1), ans + ch);
+        printSubsequenceRF(ques.substring(1), ans + "");
+
+
+    }
+
+//-------------------------------------------------------------
+
+
+
 }
