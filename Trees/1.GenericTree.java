4164b5aed058507e12571f1d3a23f0f40d2c713f
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-24 13:21:33 +0530

    Added DISPLAY GENERIC TREE program code in 1.GenericTree.java.

--- Trees/1.GenericTree.java
+++ Trees/1.GenericTree.java
@@ -2,59 +2,74 @@ import java.io.*;
 import java.util.*;
 
 class genericTree {
-    
+
     static Scanner scn = new Scanner(System.in);
-    static class Node{
+
+    static class Node {
         int data;
-        ArrayList<Node> children = new ArrayList<>();  
+        ArrayList<Node> children = new ArrayList<>();
     }
+
     public static void main(String[] args) {
         solved();
-        //Unsolved();
+        // Unsolved();
 
     }
 
     public static void solved() {
         construct();
+        display();
     }
 
-
-    //static Node root = null;
+    // static Node root = null;
     public static void construct() {
         Node root = null;
-        int[] treeVal= {10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1};
+        int[] treeVal = { 10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1 };
         Node founderNode = constructFunc(treeVal, root);
         System.out.println(founderNode.data);
     }
 
-
-    public static Node constructFunc(int[] val, Node root){
+    public static Node constructFunc(int[] val, Node root) {
         Stack<Node> st = new Stack<>();
-        for(int i = 0; i < val.length; i++){
-            if(val[i] != -1){
+        for (int i = 0; i < val.length; i++) {
+            if (val[i] != -1) {
                 Node node = new Node();
                 node.data = val[i];
-                if(st.size() == 0){
+                if (st.size() == 0) {
                     root = node;
                     st.push(node);
-                }
-                else{
+                } else {
                     st.peek().children.add(node);
                     st.push(node);
                 }
 
-            }
-            else{
+            } else {
                 st.pop();
             }
         }
         return root;
     }
-    
 
-//------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------
 
+    public static void display() {
+        Node root = null;
+        int[] treeVal = { 10, 20, -1, 30, 50, -1, 60, -1, -1, 40, -1, -1 };
+        Node founderNode = constructFunc(treeVal, root);
+        displayFunc(founderNode);
 
+    }
 
+    public static void displayFunc(Node node) {
+        System.out.print(node.data + "->");
+        for (Node child : node.children) {
+            System.out.print(child.data + ",");
+        }
+        System.out.println(".");
+
+        for (Node child : node.children) {
+            displayFunc(child);
+        }
+    }
 
 }
