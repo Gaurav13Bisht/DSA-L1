b5f154be96564fcd8d8754c99b2f5b36163bb257
Gaurav13Bisht <gaurav13bisht@gmail.com>
2020-11-29 13:54:37 +0530

    Added SortingAlgos.java file and added BUBBLESORT program code in it.

---
+++ Sorts/SortingAlgos.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+class genericTree {
+
+    static Scanner scn = new Scanner(System.in);
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        solved();
+        // Unsolved();
+
+    }
+
+    public static void solved() {
+        bubbleSort();
+    }
+
+    public static void bubbleSort() {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        bubbleSortFunc(arr);
+        print(arr);
+    }
+
+    public static void bubbleSortFunc(int[] arr) {
+        int size = arr.length;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (!isSmaller(arr, j + 1, j))
+                    continue;
+                else
+                    swap(arr, j + 1, j);
+
+            }
+            size--;
+        }
+
+    }
+
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // return true if ith element is smaller than jth element
+    public static boolean isSmaller(int[] arr, int i, int j) {
+        System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+        if (arr[i] < arr[j]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------
+
+}
